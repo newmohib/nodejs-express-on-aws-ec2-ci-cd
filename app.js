ff36047b9ec5e1fd0534a2386cda7efc0deaddf8
Mohibur Rahman <mohib@bs-23.net>
2023-03-03 23:00:49 +0600

    add: integration ci/cd for aws ec2

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Success Message");
+});
+
+app.get('/products', (req, res) => {
+    res.send([
+        {
+            productId: '101',
+            price: 100
+        },
+        {
+            productId: '102',
+            price: 102
+        }
+    ])
+    
+});
+
+app.listen(port, ()=>{
+    console.log("Demo app is up and listening to port: ", port);
+});
